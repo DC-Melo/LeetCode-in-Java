4e31f8b2efd8cb2140ec8010f0c28e2bfdb7bb0e
DC-Melo <melodachor@gmail.com>
2024-03-26 14:45:13 +0800

    add utils local jar

--- src/test/java/g0001_0100/s0002_add_two_numbers/SolutionTest.java
+++ src/test/java/g0001_0100/s0002_add_two_numbers/SolutionTest.java
@@ -5,9 +5,8 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.junit.jupiter.api.Test;
 
-import com_github_leetcode.LinkListUtils;
+import com_github_leetcode.LinkedListUtils;
 import com_github_leetcode.ListNode;
-import g0001_0100.s0002_add_two_numbers.Solution;
 
 /**
  * SolutionTest
@@ -17,8 +16,8 @@ public class SolutionTest {
   @Test
   public void addTwoNumberTest() {
 
-    ListNode l1 = LinkListUtils.contructLinkedList(new int[] { 2, 4, 3 });
-    ListNode l2 = LinkListUtils.contructLinkedList(new int[] { 5, 6, 4 });
+    ListNode l1 = LinkedListUtils.contructLinkedList(new int[] { 2, 4, 3 });
+    ListNode l2 = LinkedListUtils.contructLinkedList(new int[] { 5, 6, 4 });
     assertThat(new Solution().addTwoNumbers(l1, l2).toString(), equalTo("7, 0, 8"));
   }
 }
